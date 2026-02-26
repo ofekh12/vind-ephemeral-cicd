0e1110f8b6c373d30981f1b087e195946a8cb84d
ofekh12 <ofekharari121@gmail.com>
2026-02-26 19:29:17 +0200

    Add CI/CD pipeline

---
+++ src/main/java/com/devops/app/HelloController.java
@@ -0,0 +1,13 @@
+package com.devops.app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+        return "200 OK - Application is running!";
+    }
+}
